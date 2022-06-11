07c38da01839800519fb052c9c06fa9b24c0c0ae
armorDLV <59936969+armorDLV@users.noreply.github.com>
2022-06-11 15:33:29 +0200

    Update hack_3.py

--- hack_3.py
+++ hack_3.py
@@ -4,7 +4,7 @@ import itertools
 import string
 import urllib.request
 
-parser = argparse.ArgumentParser(description='Stage 1/5: Establishing a connection')
+parser = argparse.ArgumentParser(description='Stage 3/5: Smarter, dictionary-based brute force')
 parser.add_argument('ip', help="IP address")
 parser.add_argument('port', type=int, help="Port number")
 args = parser.parse_args()
